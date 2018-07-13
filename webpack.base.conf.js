a06befdfc5f82befeea6cd8bf2dc488ef3f35fc8
Tmjk <thomasandyjk@gmail.com>
2018-07-13 17:51:05 +0800

    simplify webpack base config file.

--- webpack.base.conf.js
+++ webpack.base.conf.js
@@ -1,12 +1,4 @@
-const path = require('path');
-
 module.exports = {
-  entry: './src/index.js',
-  mode: 'production',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
   module: {
     rules: [{
       test: /\.css$/,
