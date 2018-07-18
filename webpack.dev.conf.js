d9d7ebd736c54374f33eb1f4f2dcaec20ba35f45
Tmjk <thomasandyjk@gmail.com>
2018-07-18 10:32:07 +0800

    make webpack server work correctly

--- webpack.dev.conf.js
+++ webpack.dev.conf.js
@@ -1,7 +1,11 @@
 const merge = require('webpack-merge');
 const baseWebpackConfig = require('./webpack.base.conf');
+const path = require('path');
 
 module.exports = merge(baseWebpackConfig, {
   mode: 'development',
   devtool: 'eval-source-map',
+  serve: {
+    content: path.resolve(__dirname, 'dist'),
+  },
 });
